3d48658930da0308ea93582f84d0f348830c0dd9
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-02-24 11:58:26 -0300

    Backend teoricamente completo. Próximo passo: Integração ao Streamlit

---
+++ src/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Sistema de Estoque Ativo")
+	// Tente digitar uma função do seu database aqui embaixo
+	// Exemplo: ConectarBanco()
+	buscarLivrosAutor()
+}
